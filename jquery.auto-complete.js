167cfdcda2fcf5f3172646903711e5daea9a6a4f
SimonSteinberger <simon.steinberger@outlook.com>
2015-04-30 18:14:18 +0200

    Made code slightly smaller.

--- jquery.auto-complete.js
+++ jquery.auto-complete.js
@@ -30,7 +30,7 @@
         return this.each(function(){
             var that = $(this);
             // sc = 'suggestions container'
-            that.sc = $('<div class="autocomplete-suggestions"></div>').addClass(o.menuClass);
+            that.sc = $('<div class="autocomplete-suggestions '+o.menuClass+'"></div>');
             that.data('el', that.sc).data('autocomplete', that.attr('autocomplete'));
             that.attr('autocomplete', 'off');
             that.cache = {};
